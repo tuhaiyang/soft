f3d34403a0dbf7b57478af43d0c5620caf06e560
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2021-01-27 13:19:49 +0800

    Add files via upload

---
+++ diy-part.sh
@@ -0,0 +1,10 @@
+rm -rf package/luci-app-openclash && rm -rf package/lua-maxminddb && rm -rf package/luci-app-vssr && rm -rf package/luci-app-jd-dailybonus && rm -rf package/luci-app-dockerman && rm -rf package/luci-app-diskman && rm -rf package/luci-app-aliddns && rm -rf package/luci-app-adguardhome && rm -rf package/lean/luci-app-jd-dailybonus
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
+git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+
